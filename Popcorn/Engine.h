bcc5336273062b7802850727770801ab8b1b0482
Ivan Denisenko <rf.sm@mail.ru>
2021-07-28 22:58:38 +0700

    Prototype 3

---
+++ Popcorn/Engine.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <windows.h>
+
+void Init();
+void Draw_Frame(HDC hdc);
--- Popcorn/Main.cpp
+++ Popcorn/Main.cpp
@@ -11,14 +11,6 @@ HINSTANCE hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 
-const int Global_Scale = 3;
-const int Brick_Width = 15;
-const int Brick_Height = 7;
-const int Cell_Width = 16;
-const int Cell_Height = 8;
-const int Level_X_Offset = 8;
-const int Level_Y_Offset = 8;
-
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
@@ -26,70 +18,67 @@ LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 //------------------------------------------------------------------------------------------------------------
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+   _In_opt_ HINSTANCE hPrevInstance,
+   _In_ LPWSTR    lpCmdLine,
+   _In_ int       nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+   UNREFERENCED_PARAMETER(hPrevInstance);
+   UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: Place code here.
+   // TODO: Place code here.
 
-    // Initialize global strings
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_POPCORN, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+   // Initialize global strings
+   LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+   LoadStringW(hInstance, IDC_POPCORN, szWindowClass, MAX_LOADSTRING);
+   MyRegisterClass(hInstance);
 
-    // Perform application initialization:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+   // Perform application initialization:
+   if (!InitInstance (hInstance, nCmdShow))
+   {
+      return FALSE;
+   }
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_POPCORN));
+   HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_POPCORN));
 
-    MSG msg;
+   MSG msg;
 
-    // Main message loop:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
+   // Main message loop:
+   while (GetMessage(&msg, nullptr, 0, 0))
+   {
+      if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+      {
+         TranslateMessage(&msg);
+         DispatchMessage(&msg);
+      }
+   }
 
-    return (int) msg.wParam;
+   return (int) msg.wParam;
 }
 //------------------------------------------------------------------------------------------------------------
-
-
 //
 //  FUNCTION: MyRegisterClass()
 //
 //  PURPOSE: Registers the window class.
 //
-//------------------------------------------------------------------------------------------------------------
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
+   WNDCLASSEXW wcex;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+   wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_POPCORN));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = CreateSolidBrush(RGB(0, 0, 0));
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_POPCORN);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+   wcex.style          = CS_HREDRAW | CS_VREDRAW;
+   wcex.lpfnWndProc    = WndProc;
+   wcex.cbClsExtra     = 0;
+   wcex.cbWndExtra     = 0;
+   wcex.hInstance      = hInstance;
+   wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_POPCORN));
+   wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+   wcex.hbrBackground  = CreateSolidBrush(RGB(15, 63, 31));
+   wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_POPCORN);
+   wcex.lpszClassName  = szWindowClass;
+   wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-    return RegisterClassExW(&wcex);
+   return RegisterClassExW(&wcex);
 }
 //------------------------------------------------------------------------------------------------------------
 //
@@ -102,12 +91,13 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //        In this function, we save the instance handle in a global variable and
 //        create and display the main program window.
 //
-//------------------------------------------------------------------------------------------------------------
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // Store instance handle in our global variable
 
-   RECT window_rect; 
+   Init();
+
+   RECT window_rect;
    window_rect.left = 0;
    window_rect.top = 0;
    window_rect.right = 320 * 3;
@@ -126,41 +116,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 
    return TRUE;
 }
-//------------------------------------------------------------------------------------------------------------
-void Draw_Brick(HDC hdc, int x, int y, bool is_blue)
-{//Отрисовка кирпича
-
-   HPEN pen;
-   HBRUSH brush;
-
-   if (is_blue) 
-   {
-      pen = CreatePen(PS_SOLID, 0, RGB(85, 255, 255));
-      brush = CreateSolidBrush(RGB(85, 255, 255));
-   }
-   else 
-   {
-      pen = CreatePen(PS_SOLID, 0, RGB(255, 85, 255));
-      brush = CreateSolidBrush(RGB(255, 85, 255));
-   }
-
-   SelectObject(hdc, pen);
-   SelectObject(hdc, brush);
-
-   Rectangle(hdc, x * Global_Scale, y * Global_Scale, (x + Brick_Width) * Global_Scale, (y + Brick_Height) * Global_Scale);
-}
-//------------------------------------------------------------------------------------------------------------
-void Draw_Frame(HDC hdc) 
-{//отрисовка экрана игры
-
-   int i, j;
-
-   for (i = 0; i < 14; i++)
-      for (j = 0; j < 12; j++)
-         Draw_Brick(hdc, Level_X_Offset + j * Cell_Width, Level_Y_Offset + i * Cell_Height, TRUE);
-
-}
-//------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------ 
 //
 //  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
 //
@@ -171,67 +127,68 @@ void Draw_Frame(HDC hdc)
 //  WM_DESTROY  - post a quit message and return
 //
 //
-//------------------------------------------------------------------------------------------------------------
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Parse the menu selections:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
+   switch (message)
+   {
+   case WM_COMMAND:
+   {
+      int wmId = LOWORD(wParam);
+      // Parse the menu selections:
+      switch (wmId)
+      {
+      case IDM_ABOUT:
+         DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+         break;
+      case IDM_EXIT:
+         DestroyWindow(hWnd);
+         break;
+      default:
+         return DefWindowProc(hWnd, message, wParam, lParam);
+      }
+   }
+   break;
 
 
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Add any drawing code that uses hdc here...
-            Draw_Frame(hdc);  
-            EndPaint(hWnd, &ps);
-        }
-        break;
+   case WM_PAINT:
+   {
+      PAINTSTRUCT ps;
+      HDC hdc = BeginPaint(hWnd, &ps);
+      // TODO: Add any drawing code that uses hdc here...
+      Draw_Frame(hdc);
+      EndPaint(hWnd, &ps);
+   }
+   break;
 
 
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
+   case WM_DESTROY:
+      PostQuitMessage(0);
+      break;
+
+
+   default:
+      return DefWindowProc(hWnd, message, wParam, lParam);
+   }
+   return 0;
 }
 //------------------------------------------------------------------------------------------------------------
 // Message handler for about box.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
+   UNREFERENCED_PARAMETER(lParam);
+   switch (message)
+   {
+   case WM_INITDIALOG:
+      return (INT_PTR)TRUE;
 
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+   case WM_COMMAND:
+      if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+      {
+         EndDialog(hDlg, LOWORD(wParam));
+         return (INT_PTR)TRUE;
+      }
+      break;
+   }
+   return (INT_PTR)FALSE;
 }
 //------------------------------------------------------------------------------------------------------------
