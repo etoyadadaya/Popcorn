475fc078d73fb95fa7569bcbaaf12b530b2f0c33
Ivan Denisenko <rf.sm@mail.ru>
2021-07-29 14:45:21 +0700

    Prototype 4

--- Popcorn/Engine.cpp
+++ Popcorn/Engine.cpp
@@ -7,8 +7,8 @@ enum EBrick_Type
    EBT_Blue
 };
 
-HPEN Brick_Red_Pen, Brick_Blue_Pen;
-HBRUSH Brick_Red_Brush, Brick_Blue_Brush;
+HPEN Highlight_Pen, Brick_Red_Pen, Brick_Blue_Pen, Platform_Circle_Pen, Platform_Inner_Pen;
+HBRUSH Brick_Red_Brush, Brick_Blue_Brush, Platform_Circle_Brush, Platform_Inner_Brush;
 
 const int Global_Scale = 3;
 const int Brick_Width = 15;
@@ -17,6 +17,9 @@ const int Cell_Width = 16;
 const int Cell_Height = 8;
 const int Level_X_Offset = 8;
 const int Level_Y_Offset = 6;
+const int Circle_Size = 7;
+int Inner_Width = 21;
+
 
 char Level_01[14][12] =
 {
@@ -36,14 +39,21 @@ char Level_01[14][12] =
    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 };
 //------------------------------------------------------------------------------------------------------------
+void Create_Pen_Brush(unsigned char r, unsigned char g, unsigned char b, HPEN &pen, HBRUSH &brush)
+{
+   pen = CreatePen(PS_SOLID, 0, RGB(r, g, b));
+   brush = CreateSolidBrush(RGB(r, g, b));
+}
+//------------------------------------------------------------------------------------------------------------
 void Init()
 {// Настройка игры при старте
 
-   Brick_Red_Pen = CreatePen(PS_SOLID, 0, RGB(255, 85, 85));
-   Brick_Red_Brush = CreateSolidBrush(RGB(255, 85, 85));
+   Highlight_Pen = CreatePen(PS_SOLID, 0, RGB(255,255,255));
 
-   Brick_Blue_Pen = CreatePen(PS_SOLID, 0, RGB(85, 255, 255));
-   Brick_Blue_Brush = CreateSolidBrush(RGB(85, 255, 255));
+   Create_Pen_Brush(255, 85, 85, Brick_Red_Pen, Brick_Red_Brush);
+   Create_Pen_Brush(85, 255, 255, Brick_Blue_Pen, Brick_Blue_Brush);
+   Create_Pen_Brush(151, 0, 0, Platform_Circle_Pen, Platform_Circle_Brush);
+   Create_Pen_Brush(0, 128, 192, Platform_Inner_Pen, Platform_Inner_Brush);                                                                                                                                         
 }
 //------------------------------------------------------------------------------------------------------------
 void Draw_Brick(HDC hdc, int x, int y, EBrick_Type brick_type)
@@ -87,9 +97,33 @@ void Draw_Level(HDC hdc)
          Draw_Brick(hdc, Level_X_Offset + j * Cell_Width, Level_Y_Offset + i * Cell_Height, (EBrick_Type)Level_01[i][j]);
 }
 //------------------------------------------------------------------------------------------------------------
+void Draw_Platform(HDC hdc, int x, int y)
+{//рисуем платформу                 
+
+   //1. рисуем боковые шарики
+   SelectObject(hdc, Platform_Circle_Pen);
+   SelectObject(hdc, Platform_Circle_Brush);
+
+   Ellipse(hdc, x * Global_Scale, y * Global_Scale, (x + Circle_Size) * Global_Scale, (y + Circle_Size) * Global_Scale);
+   Ellipse(hdc, (x + Inner_Width) * Global_Scale, y * Global_Scale, (x + Circle_Size + Inner_Width) * Global_Scale, (y + Circle_Size) * Global_Scale);
+
+   //2. рисуем блик
+   SelectObject(hdc, Highlight_Pen);
+
+   Arc(hdc, (x + 1) * Global_Scale, (y + 1) * Global_Scale, (x + Circle_Size - 1) * Global_Scale, (y + Circle_Size - 1) * Global_Scale, 
+      (x + 1 + 1) * Global_Scale, (y + 1) * Global_Scale, (x + 1) * Global_Scale, (y + 1 + 2) * Global_Scale);
+   
+   //3. рисуем среднюю часть
+   SelectObject(hdc, Platform_Inner_Pen);
+   SelectObject(hdc, Platform_Inner_Brush);
+
+   RoundRect(hdc, (x + 4) * Global_Scale, (y + 1) * Global_Scale, (x + 4 + Inner_Width - 1) * Global_Scale, (y + 1 + 5) * Global_Scale, 3 * Global_Scale, 3 * Global_Scale);
+}
+//------------------------------------------------------------------------------------------------------------
 void Draw_Frame(HDC hdc)
 {// Отрисовка экрана игры
 
    Draw_Level(hdc);
+   Draw_Platform(hdc, 50, 100);
 }
 //------------------------------------------------------------------------------------------------------------
